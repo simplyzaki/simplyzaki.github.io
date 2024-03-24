7b23556023a92de07190d13b68587e0f3abd79f8
Zakiullah Khan <simplyzaki@outlook.com>
2024-03-24 16:01:12 -0500

    updated to astro cube template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.simplyzaki.com',
-  integrations: [tailwind(), sitemap(), robotsTxt(), pagefind()]
+  integrations: [sitemap()]
 });

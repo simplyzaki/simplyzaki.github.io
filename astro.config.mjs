1d3c3afc21dc5bd3640c65ffd23a993962189c8d
Zakiullah Khan <simplyzaki@outlook.com>
2024-03-24 11:00:49 -0500

    added github deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.simplyzaki.com',
   integrations: [tailwind(), sitemap()]
 });
